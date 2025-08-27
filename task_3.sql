505130b121d5a443496dc53d499671752784a942
MUKIZA Jean <jean.mukiza16@gmail.com>
2025-08-27 18:16:34 +0200

    add task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+SELECT * FROM alx_book_store.authors;
+SELECT * FROM alx_book_store.books;
+SELECT * FROM alx_book_store.customers;
+SELECT * FROM alx_book_store.order_details;
+SELECT * FROM alx_book_store.orders;
