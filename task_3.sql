f7b7479b0abeba006eec2ddf7cfb4e37e63a2540
MUKIZA Jean <jean.mukiza16@gmail.com>
2025-08-27 19:49:23 +0200

    add task_4.sql

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,7 @@
-SELECT * FROM alx_book_store.authors;
-SELECT * FROM alx_book_store.books;
-SELECT * FROM alx_book_store.customers;
-SELECT * FROM alx_book_store.order_details;
-SELECT * FROM alx_book_store.orders;
+
+ SHOW TABLES FROM alx_book_store;
+SET @db_name = 'alx_book_store';
+SET @sql = CONCAT('SHOW TABLES FROM ', @db_name);
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
