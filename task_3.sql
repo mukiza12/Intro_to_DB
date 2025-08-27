dbac282d6a3ca0ce80b77eefc7a7569bcc4dd3fa
MUKIZA Jean <jean.mukiza16@gmail.com>
2025-08-27 23:35:22 +0200

    add task_5.sql

--- task_3.sql
+++ task_3.sql
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
+-- <<<<<<< HEAD
 
  SHOW TABLES FROM alx_book_store;
-=======
+-- =======
 SHOW TABLES FROM alx_book_store;
->>>>>>> 0b0ff17aaf2d0088517eb7011536bfbc4c6283e6
+-- >>>>>>> 0b0ff17aaf2d0088517eb7011536bfbc4c6283e6
 SET @db_name = 'alx_book_store';
 SET @sql = CONCAT('SHOW TABLES FROM ', @db_name);
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
-<<<<<<< HEAD
+-- <<<<<<< HEAD
 DEALLOCATE PREPARE stmt;
-=======
+-- =======
 DEALLOCATE PREPARE stmt;
->>>>>>> 0b0ff17aaf2d0088517eb7011536bfbc4c6283e6
+-- >>>>>>> 0b0ff17aaf2d0088517eb7011536bfbc4c6283e6
